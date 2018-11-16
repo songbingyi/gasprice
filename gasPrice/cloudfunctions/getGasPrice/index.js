b3198e46f4e636549c21b7e8d5ef683af802814a
fu <43463006@qq.com>
2018-11-16 17:45:17 +0800

    gas接口 pass

--- gasPrice/cloudfunctions/getGasPrice/index.js
+++ gasPrice/cloudfunctions/getGasPrice/index.js
@@ -6,21 +6,28 @@ cloud.init()
 // 云函数入口函数
 cloud.init()
 
-exports.main = async (event, context) => {
-    let options = {
-        method: 'GET',
-        url: 'http://ali-todayoil.showapi.com/todayoil',
-        headers: {
-            'Authorization': 'APPCODE 312a0ea7c23b419e967bbf80f72cbc21'
-        }
+exports.main = async(event, context) => {
+  let options = {
+    method: 'GET',
+    url: 'http://ali-todayoil.showapi.com/todayoil',
+    formData:{
+    },
+    headers: {
+      'Authorization': 'APPCODE 312a0ea7c23b419e967bbf80f72cbc21'
     }
-    const result = rp(options).then(function (body) {
-        return body
-
-    }).catch(function (err) {
-        console.log(err)
-        return err
+  }
+  const result = rp(options).then(function(body) {
+    let gasPriceInfo = JSON.parse(body)
+    let gasPriceList = gasPriceInfo.showapi_res_body.list
+    let cityGasPriceInfo = gasPriceList.find(function (element) {
+      return element.prov == event.prov
     });
-    return result
+    return cityGasPriceInfo;
+
+  }).catch(function(err) {
+    console.log(err)
+    return err
+  });
+  return result
 
 }
